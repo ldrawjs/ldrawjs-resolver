fcc56f9b5a9dc416beef0be2ce55b69b36bf4a49
Ziv <ziv.perry@zoominfo.com>
2023-02-12 17:26:17 +0200

    db as code

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
+import DB from "./db.ts";
 
-const DB = (await import("./db.json", {
-  assert: { type: "json" },
-})).default as Record<string, string>;
 
 function handler(req: Request) {
   const name = (new URL(req.url).pathname).substring(1);
